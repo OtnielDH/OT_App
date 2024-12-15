980a925b4472832211dbf4eefc76ecdacae50067
Otniel Dwimartin Hutapea <otnieldwimartin21@gmail.com>
2024-12-15 08:25:58 +0700

    Correct icon import at vue main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,15 +3,17 @@ import App from './App.vue'
 import router from './router'
 
 // CoreUI
-import CoreuiVue from '@coreui/vue'
-import CIcon from '@coreui/icons-vue'
-import { iconsSet as icons } from '@coreui/icons'
 import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "@coreui/icons/css/all.min.css"
+import CoreuiVue from '@coreui/vue'
+import CIcon from '@coreui/icons-vue'
+import * as icons from '@coreui/icons'
+
 
 const app = createApp(App)
 app.use(router)
-app.component('CIcon', CIcon)
 app.provide('icons', icons)
+app.component('CIcon', CIcon)
 app.use(CoreuiVue)
 app.mount('#app')
